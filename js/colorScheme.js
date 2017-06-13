29a2ef33edbaed8381965e53debe2f7d1ef592ee
stuicey <40115468@live.napier.ac.uk>
2017-06-13 16:15:01 +0100

    Adds ColourSchemes & Loading animation
    - Adds a colour scheme option for the terminal
    - Adds a loading animation while connecting

---
+++ js/colorScheme.js
@@ -0,0 +1,56 @@
+var colorScheme_tango = true  // tango is the default scheme
+var colorScheme_solarized = ["#002b36","#002b36","#dc322f","#859900","#b58900","#268bd2","#6c71c4","#2aa198","#93a1a1","#657b83","#dc322f","#859900","#b58900","#268bd2","#6c71c4","#2aa198","#fdf6e3", "#93a1a1", "#93a1a1"]
+var colorScheme_monokai = ["#272822","#48483e","#dc2566","#8fc029","#d4c96e","#55bcce","#9358fe","#56b7a5","#acada1","#76715e","#fa2772","#a7e22e","#e7db75","#66d9ee","#ae82ff","#66efd5","#cfd0c2", "#f1ebeb",  "#f1ebeb"]
+var colorScheme_ashes = ["#1c2023","#1c2023","#c7ae95","#95c7ae","#aec795","#ae95c7","#c795ae","#95aec7","#c7ccd1","#747c84","#c7ae95","#95c7ae","#aec795","#ae95c7","#c795ae","#95aec7","#f3f4f5", "#c7ccd1", "#c7ccd1"]
+
+var colorSchemes = ['', colorScheme_solarized, colorScheme_monokai, colorScheme_ashes]
+
+// format (hex) - [bg,0,1,2,3 ... 14,15, cur, fg]
+function set_colorscheme(colors){
+	var term_style = document.styleSheets[0]
+	// Remove active colour scheme if there is a custom one.
+	if(!colorScheme_tango){
+		for(var i = 0; i < 33; i++){
+			term_style.removeRule(13)
+		}
+		// Tango is the default color scheme so nothing more is needed
+		if(!colors){
+			colorScheme_tango = true
+			return
+		}
+	}
+
+	term_style.insertRule('.terminal .terminal-cursor {background-color: ' + colors[17] + '; color: ' + colors[0] +' !important;}', 13)
+	term_style.insertRule('.terminal:not(.focus) .terminal-cursor {outline: 1px solid ' + colors[17] + ' !important;}', 13)
+	term_style.insertRule('.terminal .xterm-viewport {background-color: ' + colors[0] + ' !important;}', 13)
+	term_style.insertRule('html, body {background-color: '+ colors[0] + ' !important;}', 13)
+	term_style.insertRule('.terminal {color: ' + colors[18] + ' !important;}', 13)
+
+	for(var i = 1; i < 16; i++){
+		term_style.insertRule('.terminal .xterm-color-' + (i - 1) + ' {color: ' + colors[i] + ' !important;}', 13)
+		term_style.insertRule('.terminal .xterm-bg-color-' + (i - 1) + ' {background-color: ' + colors[i] + ' !important;}', 13)
+	}
+
+	colorScheme_tango = false
+}
+
+function importXresources(){
+	var file = document.getElementById('Xresources').value
+	var lines = file.split("\n")
+	// natural sort the Xresources list to bg, 1 - 15, cur, fg colours & slice the leading 18 lines (!blue ect)
+	lines = lines.sort(new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'}).compare).slice(18)
+
+
+	colorScheme_custom = []
+
+	for(var i = 0; i < lines.length; i++){
+		colorScheme_custom.push("#" + lines[i].split("#")[1])
+	}
+
+	set_colorscheme(colorScheme_custom)
+}
+
+var c = 0
+function cycleColorSchemes(){
+	set_colorscheme(colorSchemes[++c > colorSchemes.length - 1 ? c = 0 : c])
+}
